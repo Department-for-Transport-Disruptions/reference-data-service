6919defe1f1f7190a616c34c90105af8c29f7777
lj-kpmg <122358913+lj-kpmg@users.noreply.github.com>
2023-04-28 11:45:17 +0100

    feat: add pipeline to deploy to pre-prod

--- stacks/S3.ts
+++ stacks/S3.ts
@@ -1,6 +1,5 @@
-import { Duration } from "aws-cdk-lib";
 import { BucketEncryption } from "aws-cdk-lib/aws-s3";
-import { Bucket, Stack, StackContext } from "sst/constructs";
+import { Bucket, Stack, StackContext, toCdkDuration } from "sst/constructs";
 
 export function S3Stack({ stack }: StackContext) {
     const csvBucket = createS3Bucket(stack, "csv-data");
@@ -26,7 +25,9 @@ const createS3Bucket = (stack: Stack, name: string, expirationDays?: number) =>
                     restrictPublicBuckets: true,
                 },
                 encryption: BucketEncryption.S3_MANAGED,
-                lifecycleRules: expirationDays ? [{ enabled: true, expiration: Duration.days(expirationDays) }] : [],
+                lifecycleRules: expirationDays
+                    ? [{ enabled: true, expiration: toCdkDuration(`${expirationDays} days`) }]
+                    : [],
             },
         },
     });
