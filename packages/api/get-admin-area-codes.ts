2fd16be5097f6d6bf3e9cf2acc1ab50f79c5fd19
alvak1 <karthik.alva@kpmg.co.uk>
2023-06-20 15:55:42 +0100

    Added change as per review comment

--- packages/api/get-admin-area-codes.ts
+++ packages/api/get-admin-area-codes.ts
@@ -5,7 +5,6 @@ import { executeClientWithoutInput } from "./execute-client";
 export const main = async (event: APIGatewayEvent): Promise<APIGatewayProxyResultV2> =>
     executeClientWithoutInput(event, getAdminAreaCodes, formatService);
 
-// eslint-disable-next-line @typescript-eslint/require-await
 export const formatService = async (areaCodes: AdminAreaCodes): Promise<(string | null)[]> => {
-    return areaCodes.map((areaCode) => areaCode.administrativeAreaCode).sort();
+    return await Promise.all(areaCodes.map((areaCode) => areaCode.administrativeAreaCode).sort());
 };
