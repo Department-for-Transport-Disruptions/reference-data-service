e831f09176ab0b57a5242161d99eb0940cbe105d
Laurence Jones <laurence@ljtech.solutions>
2023-07-21 14:26:53 +0100

    added indexes to localities table

---
+++ services/migrations/1689935058-localities_index.mjs
@@ -0,0 +1,17 @@
+import { Kysely } from "kysely";
+
+/**
+ * @param db {Kysely<any>}
+ */
+export async function up(db) {
+    await db.schema.createIndex("idx_nptgLocalityCode").on("stops").column("nptgLocalityCode").execute();
+    await db.schema.createIndex("idx_nptgLocalityCode").on("localities").column("nptgLocalityCode").execute();
+}
+
+/**
+ * @param db {Kysely<any>}
+ */
+export async function down(db) {
+    await db.schema.dropIndex("idx_nptgLocalityCode").on("stops").execute();
+    await db.schema.dropIndex("idx_nptgLocalityCode").on("localities").execute();
+}
