ccadfcca12a948e770c4b1d2006a949864c03731
Deanna Sharma <deanna.sharma@doza.consulting>
2023-10-03 14:33:09 +0100

    lint

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
     test: {
         include: ["**/*.{test,spec}.{js,ts,tsx}"],
-        poolOptions: {},
+        threads: false,
         restoreMocks: true,
     },
 });
