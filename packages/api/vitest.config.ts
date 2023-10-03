7ce041beeb5923232a7653e00effb58c6e976e81
Deanna Sharma <deanna.sharma@doza.consulting>
2023-10-03 11:39:56 +0100

    created vitest config file

---
+++ packages/api/vitest.config.ts
@@ -0,0 +1,9 @@
+import { defineConfig } from "vitest/config";
+
+export default defineConfig({
+    test: {
+        include: ["**/*.{test,spec}.{js,ts,tsx}"],
+        threads: false,
+        restoreMocks: true,
+    },
+});
