0a12ed52096fd109c60e80ea14b765fccc59c509
Deanna Sharma <83244002+DeannaSharma@users.noreply.github.com>
2023-11-30 17:08:44 +0000

    fix: increase workslocationcoordinates length to 10000 (#74)

---
+++ services/migrations/1701362090-roadworks_worksLocationCoordinates_length.mjs
@@ -0,0 +1,15 @@
+import { Kysely, sql } from "kysely";
+
+/**
+ * @param db {Kysely<any>}
+ */
+export async function up(db) {
+    await sql`ALTER TABLE roadworks MODIFY worksLocationCoordinates VARCHAR(10000)`.execute(db);
+}
+
+/**
+ * @param db {Kysely<any>}
+ */
+export async function down(db) {
+    await sql`ALTER TABLE roadworks MODIFY worksLocationCoordinates VARCHAR(255)`.execute(db);
+}
