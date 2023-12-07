f8a02b1f1139a5a7ca51806b48f8ecc20d8ce3f3
Alaina Jaffer <59929701+alainajaffer@users.noreply.github.com>
2023-12-07 15:33:50 -0800

    feat: Cleanup data roadworks (#77)

---
+++ packages/ref-data-uploaders/cleanup-roadworks/index.ts
@@ -0,0 +1,73 @@
+import { Database, getDbClient, waitForDb } from "@reference-data-service/core/db";
+import { randomUUID } from "crypto";
+import * as logger from "lambda-log";
+import { Kysely, sql } from "kysely";
+
+export const deleteOldRoadworks = async (dbClient: Kysely<Database>) => {
+    await dbClient
+        .deleteFrom("roadworks")
+        .where((qb) =>
+            qb.or([
+                qb.and([
+                    qb(
+                        sql`DATE_ADD(DATE_FORMAT(left(actualEndDateTime,10),'%Y-%m-%d'), INTERVAL 7 DAY)`,
+                        "<",
+                        sql`CURDATE()`,
+                    ),
+                    qb("permitStatus", "=", "closed"),
+                ]),
+                qb.and([
+                    qb(
+                        sql`DATE_ADD(DATE_FORMAT(left(lastUpdatedDateTime,10),'%Y-%m-%d'), INTERVAL 7 DAY)`,
+                        "<",
+                        sql`CURDATE()`,
+                    ),
+                    qb.or([
+                        qb("permitStatus", "=", "revoked"),
+                        qb("permitStatus", "=", "refused"),
+                        qb("permitStatus", "=", "cancelled"),
+                    ]),
+                ]),
+            ]),
+        )
+        .execute();
+};
+
+export const main = async () => {
+    logger.options.dev = process.env.NODE_ENV !== "production";
+    logger.options.debug = process.env.ENABLE_DEBUG_LOGS === "true" || process.env.NODE_ENV !== "production";
+
+    logger.options.meta = {
+        id: randomUUID(),
+    };
+
+    const dbClient = getDbClient();
+
+    try {
+        await waitForDb(dbClient);
+
+        logger.info("Starting Cleanup roadworks");
+
+        await deleteOldRoadworks(dbClient);
+
+        logger.info("Cleanup roadworks complete");
+    } catch (e) {
+        if (e instanceof Error) {
+            logger.error(e);
+
+            return {
+                statusCode: 500,
+                body: JSON.stringify({
+                    error: "There was a problem with cleanup roadworks",
+                }),
+            };
+        }
+
+        return {
+            statusCode: 500,
+            body: JSON.stringify({
+                error: "There was a problem with cleanup roadworks",
+            }),
+        };
+    }
+};
